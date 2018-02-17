8a17dd4bfd240bd0353469c5bee1319cc44c8cf8
Alberto Jose Garcia Gago <albertoggago@gmail.com>
2018-02-17 17:49:50 +0000

    docker jenkins v3

--- pythonTest.sh
+++ pythonTest.sh
@@ -4,5 +4,5 @@ echo $0
 #py.test testanalyzerwebjobsTextFuntions.py -v 
 #py.test testa*.py testl*.py testm*.py testr*.py testt*.py -v 
 #py.test testa*.py testt*.py -v 
-py.test testt*.py -v
+py.test test*.py -v
 #py.test testr*.py testt*.py -v 
