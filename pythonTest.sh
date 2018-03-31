8886c958b7ad24eb301216f986d79a69db64f94d
Alberto Jose Garcia Gago <albertoggago@gmail.com>
2018-03-31 22:23:20 +0100

    jenkins+selenium in docker

--- pythonTest.sh
+++ pythonTest.sh
@@ -2,7 +2,7 @@ cd pythonBatch/test
 echo $0
 #py.test test*.py -v
 #py.test testanalyzerwebjobsTextFuntions.py -v 
-py.test --junitxml results.xml testlog*.py -v 
+py.test --junitxml results.xml test*.py -v 
 #py.test testa*.py testl*.py testm*.py testr*.py testt*.py -v 
 #py.test testa*.py testt*.py -v 
 #py.test --junitxml results.xml test*.py -v
