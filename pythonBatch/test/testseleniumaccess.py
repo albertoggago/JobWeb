5a9b472127cd9a4d7191effd8037a41019417c09
Alberto Jose Garcia Gago <albertoggago@gmail.com>
2018-04-04 19:34:25 +0100

    divide classes clean code

--- pythonBatch/test/testseleniumaccess.py
+++ pythonBatch/test/testseleniumaccess.py
@@ -1,30 +1,28 @@
+"""Test Selenium Access"""
 import sys
-import os
 import json
-import datetime
-import pymongo
-import pytest
-
-sys.path.insert(0, "..")
-from pyproj.analyzerwebjobs import AnalyzerWebJobs
-from pyproj.seleniumaccess import SeleniumAccess
-
 import urllib2
 from selenium.common.exceptions import WebDriverException
 
-def test_selenium_Ok():
-    configText = open("../test/config/configOk.json","r").read()
-    allconfig  = json.loads(configText)
-    config     = allconfig.get("webPagesDef",None)
+sys.path.insert(0, "..")
+from pyproj.seleniumaccess import SeleniumAccess
+
+
+def test_selenium_ok():
+    """test_selenium_Ok"""
+    config_text = open("../test/config/configOk.json", "r").read()
+    allconfig = json.loads(config_text)
+    config = allconfig.get("webPagesDef", None)
     seleniumaccess = SeleniumAccess(config, "DEBUG")
     seleniumaccess.open_selenium()
     assert seleniumaccess.driver != None
     seleniumaccess.close_selenium()
 
-def test_selenium_Error():
-    configText = open("../test/config/configMongoDBError.json","r").read()
-    allconfig  = json.loads(configText)
-    config     = allconfig.get("webPagesDef",None)
+def test_selenium_error():
+    """test_selenium_Error"""
+    config_text = open("../test/config/configMongoDBError.json", "r").read()
+    allconfig = json.loads(config_text)
+    config = allconfig.get("webPagesDef", None)
     seleniumaccess = SeleniumAccess(config, "DEBUG")
     try:
         seleniumaccess.open_selenium()
@@ -33,34 +31,37 @@ def test_selenium_Error():
         assert True
 
 def test_selenium_close():
-    configText = open("../test/config/configOk.json","r").read()
-    allconfig  = json.loads(configText)
-    config     = allconfig.get("webPagesDef",None)
+    """test_selenium_close"""
+    config_text = open("../test/config/configOk.json", "r").read()
+    allconfig = json.loads(config_text)
+    config = allconfig.get("webPagesDef", None)
     seleniumaccess = SeleniumAccess(config, "DEBUG")
     seleniumaccess.open_selenium()
     seleniumaccess.close_selenium()
-    assert seleniumaccess.driver == None
+    assert seleniumaccess.driver is None
 
-def test_selenium_Access_Driver_Ok():
-    configText = open("../test/config/configOk.json","r").read()
-    allconfig  = json.loads(configText)
-    config     = allconfig.get("webPagesDef",None)
+def test_driver_ok():
+    """test_Driver_Ok"""
+    config_text = open("../test/config/configOk.json", "r").read()
+    allconfig = json.loads(config_text)
+    config = allconfig.get("webPagesDef", None)
     seleniumaccess = SeleniumAccess(config, "DEBUG")
     seleniumaccess.open_selenium()
-    try :
+    try:
         seleniumaccess.driver.get("http://www.google.es")
         assert True
     except WebDriverException:
         assert False
     seleniumaccess.close_selenium()
 
-def test_selenium_Access_Driver_Malformed_URL():
-    configText = open("../test/config/configOk.json","r").read()
-    allconfig  = json.loads(configText)
-    config     = allconfig.get("webPagesDef",None)
+def test_driver_malformed_url():
+    """test_Driver_Malformed_URL"""
+    config_text = open("../test/config/configOk.json", "r").read()
+    allconfig = json.loads(config_text)
+    config = allconfig.get("webPagesDef", None)
     seleniumaccess = SeleniumAccess(config, "DEBUG")
     seleniumaccess.open_selenium()
-    try :
+    try:
         seleniumaccess.driver.get("www.google.es")
         assert False
     except WebDriverException:
